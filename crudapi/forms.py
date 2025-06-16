f30faf7cac4dc7da30d476a70d672a0625909f7a
rakhikhinder <rakhikhinder@yahoo.com>
2025-06-16 14:11:34 +0530

    media

--- crudapi/forms.py
+++ crudapi/forms.py
@@ -1,11 +1,12 @@
 # forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User, Teacher, Student,Comment, Product,Subscription
+from .models import User, Teacher, Student ,Comment ,Subscription
 
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=30)
     is_teacher = forms.BooleanField(required=False, label='Register as Teacher')
     is_student = forms.BooleanField(required=False, label='Register as Student')
     
@@ -14,26 +15,24 @@ class UserRegisterForm(UserCreationForm):
         fields = [ 'username','email', 'password1', 'password2', 'is_teacher', 'is_student']
 
 class TeacherProfileForm(forms.ModelForm):
+    
     class Meta:
         model = Teacher
-        fields = ['specialty', 'bio']
+        fields = '__all__'
+              
 
 class StudentProfileForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['grade_level', 'interests']
-    
+        fields = '__all__'
+ 
+       
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body']
-        
-        
-        
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = '__all__'
+
+
 class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
--- crudapi/models.py
+++ crudapi/models.py
@@ -2,26 +2,42 @@
 # Expert , admin and student models 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.utils import timezone
 class User(AbstractUser):
     is_teacher = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    specialty = models.CharField(max_length=100)
-    bio = models.TextField()
+    prof_img = models.ImageField(upload_to= 'teacher/',   blank=True, null=True)
+    qualification = models.CharField(max_length=500, null=True)
+    experience = models.IntegerField(null=True)
+    subject = models.CharField(max_length=200,  null=True)
+    phone = models.IntegerField(null=True)
+    created = models.DateTimeField(default=timezone.now)
+
     
     def __str__(self):
         return self.user.username
 
+
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    grade_level = models.CharField(max_length=50)
-    interests = models.TextField()
+    prof_img = models.ImageField(upload_to= 'student/',  blank=True, null=True)
+    college = models.CharField(max_length = 300,  null=True)
+    address = models.CharField(max_length= 500,  null=True)
+    state = models.CharField(max_length= 400,  null=True)
+    Country = models.CharField(max_length= 400, null=True)
+    city = models.CharField(max_length= 500,  null=True)
+    pin_code = models.IntegerField(null=True)
+    phone = models.IntegerField(null=True)
+    created = models.DateTimeField(default=timezone.now)
+
     
     def __str__(self):
         return self.user.username
+
+
 
 class Category(models.Model):
 	name = models.CharField(max_length=50)
