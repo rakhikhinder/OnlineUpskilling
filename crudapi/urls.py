a385a8234e048e15863e25aa3a858d408f7c9f42
rakhikhinder <rakhikhinder@yahoo.com>
2025-06-17 20:38:47 +0530

    sub

--- crudapi/urls.py
+++ crudapi/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('profile/complete/', views.profile_complete, name='profile_complete'),
     path('profile/show/', views.profile_show, name="profile_show"), 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/',views.logout, name='logout'),
     path('product/<int:pk>/', views.product, name='product'),
     path('', views.dashboard, name="dashboard"),
     path('create-payment-intent/', views.create_payment_intent, name='create-payment-intent'),
