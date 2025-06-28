ca4340ec6961642ec9cca7e96c437fb396647434
rakhikhinder <98300258+rakhikhinder@users.noreply.github.com>
2025-06-28 14:43:26 +0530

    Revert "Main"

--- crudapi/admin.py
+++ crudapi/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import User, Student, Teacher,Category,Product,Subscription
+from .models import User, Student, Teacher
 
 admin.site.register(User)
 admin.site.register(Student)
 admin.site.register(Teacher)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Subscription)
