eb2f9df951c9785e941944cf3c4a28a728f7e808
rakhikhinder <rakhikhinder@yahoo.com>
2025-06-28 14:10:31 +0530

    Prepare for Heroku deployment

--- crud/settings.py
+++ crud/settings.py
@@ -124,7 +124,9 @@ if os.environ.get('USE_AWS'):
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
